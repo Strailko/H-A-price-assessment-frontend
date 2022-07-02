cebc2f6f6dd0e7f57ff6ce980f77b615782f2ff8
Mile Jankuloski <59649491+Strailko@users.noreply.github.com>
2022-07-02 18:14:50 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,16 +5,15 @@ function requireHTTPS(req, res, next) {
     }
     next();
 }
+
 const express = require('express');
 const app = express();
 
 app.use(requireHTTPS);
+app.use(express.static('./dist/frontend'));
 
-app.use(express.static(’./dist/frontend’));
-
-app.get("/*", function(req, res) {
-  res.sendFile(’index.html’, {root: 'dist/frontend’}
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/frontend/'}),
 );
-});
 
 app.listen(process.env.PORT || 8080);
